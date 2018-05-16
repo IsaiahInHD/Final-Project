39ee1094b8239aac01118fc3f8ab0f1b99f22801
kochtape <38923335+kochtape@users.noreply.github.com>
2018-05-16 11:30:54 -0700

    Add files via upload

---
+++ 510/particle_system.js
@@ -0,0 +1,24 @@
+function particleSystem(loc, clr){
+  this.particles = [];
+  this.loc = loc;
+  this.clr = clr;
+
+  this.addParticle = function(){
+     this.particles.push(new Particle(this.loc, this.clr));
+  };
+
+  this.run = function(){
+     for(var i = this.particles.length-1; i >=0; i--){
+       var p = this.particles[i];
+       p.update();
+       if (p.isDead()) {
+         this.particles.splice(i, 1);
+       }
+
+     }
+
+  }
+
+
+
+}
