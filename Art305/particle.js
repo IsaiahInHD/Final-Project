22f48cf589057a22c8d5a0ac82f3fb3b97aeaca0
Isaiah <BoltTheNinja@gmail.com>
2019-03-07 08:33:06 -0800

    sa

---
+++ Art305/particle.js
@@ -0,0 +1,46 @@
+
+
+function Particle(loc, clr){
+  this.acc = createVector(random(-.005, .005), random(-.005, .005));
+  this.vel = createVector(random(-1, 1), random(-1, 1));
+  this.loc = loc.copy();
+  this.lifespan = 450.0;
+  this.clr = clr;
+
+  this.update = function(){
+    this.vel.add(this.acc);
+    this.vel.limit(5);
+    this.loc.add(this.vel);
+    this.lifespan -= 1;
+    this.render();
+  };
+
+
+  this.render = function(){
+    stroke(127, 50, 190, this.lifespan);
+    //stroke(r, g, b, this.lifespan);
+    //line(30, 20, 85, 75);
+    strokeWeight(3.5);
+    fill(this.clr);
+    ellipse(this.loc.x, this.loc.y, 12, 12);
+
+  };
+
+
+  this.isDead = function(){
+    if (this.lifespan < 0.0)  {
+      return true;
+    } else {
+
+      return false;
+
+    }
+
+
+
+  }
+
+  /*if(this.loc(copy).dist(particleSystem.loc) < 100) {
+    line(30, 20, 85, 75);
+    stroke(r, g, b);
+  */}
